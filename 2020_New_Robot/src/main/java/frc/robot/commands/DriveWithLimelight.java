9fc0b2d401ea1982af3db25dead8c70695632b71
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2020-01-29 19:56:42 -0500

    Stuff Tested On New Robot

---
+++ 2020_New_Robot/src/main/java/frc/robot/commands/DriveWithLimelight.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.LimeLight;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class DriveWithLimelight extends PIDCommand {
+  /**
+   * Creates a new DriveWithLimelight.
+   */
+  public DriveTrain driveTrain;
+  public LimeLight limelight;
+  public DriveWithLimelight() {
+    super(
+        // The controller that the command will use
+        new PIDController(Constants.driveTrainP, Constants.driveTrainI, Constants.driveTrainD),
+        // This should return the measurement
+        () -> LimeLight.getRotation(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> {DriveTrain.arcadeDrive(0, output);
+          // Use the output here
+        });
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ 2020_New_Robot/src/main/java/frc/robot/commands/TurnWithLimelight.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.LimeLight;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class TurnWithLimelight extends PIDCommand {
+  /**
+   * Creates a new TurnWithLimelight.
+   */
+  public TurnWithLimelight() {
+    super(
+        // The controller that the command will use
+        new PIDController(0.07, 0.055, 0),
+        // This should return the measurement
+        () -> LimeLight.getLimelightXOffset(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> {DriveTrain.arcadeDrive(0, output);
+          // Use the output here
+        });
+        getController().setTolerance(1);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

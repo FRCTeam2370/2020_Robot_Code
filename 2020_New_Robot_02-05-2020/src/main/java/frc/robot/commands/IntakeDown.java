3ad2da345d3bcf0375dd81f33be3b4241657d556
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2020-02-05 18:51:35 -0500

    Added Intake Support

---
+++ 2020_New_Robot_02-05-2020/src/main/java/frc/robot/commands/IntakeDown.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+
+public class IntakeDown extends CommandBase {
+  /**
+   * Creates a new IntakeDown.
+   */
+  private Intake intake = new Intake();
+  public IntakeDown() {
+    addRequirements(intake);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+// Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+    Intake.IntakeDown();
+    Timer.delay(1);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Intake.IntakeHang();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
--- 2020_New_Robot_02-05-2020/src/main/java/frc/robot/subsystems/Intake.java
+++ 2020_New_Robot_02-05-2020/src/main/java/frc/robot/subsystems/Intake.java
@@ -20,8 +20,10 @@ public class Intake extends SubsystemBase {
   public Intake() {
 
   }
-  public static double intakeSpeed = 0.5;
+  public static double intakeSpeed = .2;
+  public static double intakeUpDownSpeed = 0.1;
   public static WPI_TalonSRX intakeMotor = new WPI_TalonSRX(Constants.IntakeMotor);
+  public static WPI_TalonSRX intakeUpAndDown = new WPI_TalonSRX(Constants.IntakeArmMotor);
 
   public static void Intakein(){
     intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
@@ -33,6 +35,17 @@ public class Intake extends SubsystemBase {
     intakeMotor.stopMotor();
   }
 
+  public static void IntakeUp() {
+    intakeUpAndDown.set(ControlMode.PercentOutput, intakeUpDownSpeed);
+  }
+
+  public static void IntakeDown() {
+    intakeUpAndDown.set(ControlMode.PercentOutput, -intakeUpDownSpeed);
+  }
+
+  public static void IntakeHang() {
+    intakeUpAndDown.stopMotor();
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run

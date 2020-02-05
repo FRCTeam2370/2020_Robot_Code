b92f8f03a13e8d9240948621b223effa00b395df
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2020-02-05 18:29:33 -0500

    Added Intake

---
+++ 2020_New_Robot_02-05-2020/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+  /**
+   * Creates a new Intake.
+   */
+  public Intake() {
+
+  }
+  public static double intakeSpeed = 0.5;
+  public static WPI_TalonSRX intakeMotor = new WPI_TalonSRX(Constants.IntakeMotor);
+
+  public static void Intakein(){
+    intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
+  }
+  public static void IntakeOut(){
+    intakeMotor.set(ControlMode.PercentOutput, -intakeSpeed);
+  }
+  public static void StopIntake(){
+    intakeMotor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

57a3d998b2c6a0de7d0ccb1a11d25c0f29ed3d24
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2020-02-19 14:01:46 -0500

    Deleted Color Sensor
    Moved to New File

--- 2020_Master_Build/src/main/java/frc/robot/RobotContainer.java
+++ 2020_Master_Build/src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.AlignAndShoot;
 import frc.robot.commands.AllShoot;
 import frc.robot.commands.AutoDrivePID;
 import frc.robot.commands.AutoShootLoadPos1;

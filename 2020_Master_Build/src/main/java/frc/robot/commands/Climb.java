096e4da1de7435c848529c87b0cffbb3028d31c8
MaxMcCalla <36572894+MaxMcCalla@users.noreply.github.com>
2020-02-21 13:59:21 -0500

    I just deleted an import. Super helpful and efficient. :)

--- 2020_Master_Build/src/main/java/frc/robot/commands/Climb.java
+++ 2020_Master_Build/src/main/java/frc/robot/commands/Climb.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj.RobotState;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;

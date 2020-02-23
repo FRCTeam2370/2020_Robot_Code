aa3e42d81f12f5643f43bc9712e81bc0cfcc0b67
Alex Stute <59628278+Astute-2319@users.noreply.github.com>
2020-02-23 14:35:09 -0500

    Climber Setpoint Work

--- 2020_Master_Build/src/main/java/frc/robot/commands/ClimberToNextSetpoint.java
+++ 2020_Master_Build/src/main/java/frc/robot/commands/ClimberToNextSetpoint.java
@@ -8,12 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
 
 public class ClimberToNextSetpoint extends CommandBase {
   /**
    * Creates a new ClimberToNextSetpoint.
    */
-  public ClimberToNextSetpoint() {
+  public ClimberToNextSetpoint(Climber c) {
+    addRequirements(c);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -25,6 +27,7 @@ public class ClimberToNextSetpoint extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    
   }
 
   // Called once the command ends or is interrupted.
